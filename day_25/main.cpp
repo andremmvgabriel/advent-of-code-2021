603fa098b8a8c40a40c8672090f91a8d53c9a774
André Gabriel <andremmvgabriel@gmail.com>
2023-03-12 15:56:46 +0000

    Day 25 Part I.

--- day_25/main.cpp
+++ day_25/main.cpp
@@ -8,97 +8,95 @@
 #include "../common/utils.hpp"
 
 // template<typename T>
-using Matrix = std::vector<std::vector<char>>;
+using Matrix = std::vector<std::vector<uint8_t>>;
 
-Matrix parse_inputs(int argc, char* argv[]) {
+std::tuple<Matrix, Matrix> parse_inputs(int argc, char* argv[]) {
     // Makes sure a input file is specified
     if (argc <= 1) throw std::invalid_argument("You have to specify an input text file.");
 
     // Actual outputs
-    Matrix map;
+    Matrix e_map, s_map;
 
     // Open input file
     std::ifstream input_file(std::string(argv[1]), std::ios::in);
 
     std::string line;
     while (std::getline(input_file, line)) {
-        std::vector<char> line_nums;
+        std::vector<uint8_t> e_line, s_line;
         for (const auto & c : line) {
-            line_nums.push_back(c);
+            if (c == '>') {
+                e_line.push_back(1);
+                s_line.push_back(0);
+            } else if (c == 'v') {
+                e_line.push_back(0);
+                s_line.push_back(1);
+            } else {
+                e_line.push_back(0);
+                s_line.push_back(0);
+            }
         }
-        map.push_back(line_nums);
+        e_map.push_back(e_line);
+        s_map.push_back(s_line);
     }
 
-    return map;
+    return {e_map, s_map};
 }
 
 
 
 int main(int argc, char* argv[]) {
     // Common
-    const auto MAP = parse_inputs(argc, argv);
+    const auto [E_MAP, S_MAP] = parse_inputs(argc, argv);
 
     // Part One algorithms
     int result_1 = 0;
 
     float part_1_elapsed_time = time_block( [&](){
-        // Copy of the map
-        Matrix map_1 = MAP;
+        // Copy of the maps
+        Matrix e_map = E_MAP, s_map = S_MAP;
 
-        std::vector<std::string> prints (map_1.size(), "");
+        std::vector<std::string> prints (e_map.size(), "");
 
         while (true) {
             // Control variables
             uint8_t moved = 0x00;
 
-            for (int y = 0 ; y < map_1.size() ; y++) {
-                for (int x = 0 ; x < map_1[y].size() ; x++) {
-                    prints[y].push_back(map_1[y][x]);
-                }
-                prints[y].push_back('\t');
-            }
-
-            // New Map
-            Matrix next_state (map_1.size(), std::vector<char>(map_1[0].size(), '.'));
+            // Maps for next moves
+            Matrix next_e_map (e_map.size(), std::vector<uint8_t>(e_map[0].size(), 0));
+            Matrix next_s_map (s_map.size(), std::vector<uint8_t>(s_map[0].size(), 0));
 
             // Move the East arrows
-            for (int y = 0 ; y < map_1.size() ; y++) {
-                for (int x = 0 ; x < map_1[y].size() ; x++) {
-                    if (map_1[y][x] == '>') {
-                        if (map_1[y][(x+1)%map_1[y].size()] == '.') {
-                            next_state[y][(x+1)%map_1[y].size()] = '>';
+            for (int y = 0 ; y < e_map.size() ; y++) {
+                for (int x = 0 ; x < e_map[y].size() ; x++) {
+                    if (e_map[y][x]) {
+                        if (!e_map[y][(x+1)%e_map[y].size()] && !s_map[y][(x+1)%s_map[y].size()]) {
+                            next_e_map[y][(x+1)%e_map[y].size()] = 1;
                             moved |= 1;
                         } else {
-                            next_state[y][x] = '>';
+                            next_e_map[y][x] = 1;
                         }
                     }
                 }
             }
+            e_map = next_e_map;
 
             // Move the South arrows
-            for (int y = 0 ; y < map_1.size() ; y++) {
-                for (int x = 0 ; x < map_1[y].size() ; x++) {
-                    if (map_1[y][x] == 'v') {
-                        if (map_1[(y+1)%map_1.size()][x] == '.') {
-                            next_state[(y+1)%map_1.size()][x] = 'v';
+            for (int y = 0 ; y < s_map.size() ; y++) {
+                for (int x = 0 ; x < s_map[y].size() ; x++) {
+                    if (s_map[y][x]) {
+                        if (!e_map[(y+1)%e_map.size()][x] && !s_map[(y+1)%e_map.size()][x]) {
+                            next_s_map[(y+1)%s_map.size()][x] = 1;
                             moved |= 1;
                         } else {
-                            next_state[y][x] = 'v';
+                            next_s_map[y][x] = 1;
                         }
                     }
                 }
             }
-
-            map_1 = next_state;
+            s_map = next_s_map;
 
             result_1++;
-            // printf(">%d\n", result_1);
-            if (result_1 >= 7) break;
             if (!moved) break;
-        }
-
-        for (int i = 0 ; i < prints.size() ; i++) {
-            printf("%s\n", prints[i].c_str());
         }
     });
     
