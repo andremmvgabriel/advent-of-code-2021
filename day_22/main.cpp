5ca3cfb7a2843cf8e920547071e6906494fce3f3
André Gabriel <andremmvgabriel@gmail.com>
2023-03-19 22:56:11 +0000

    Day 22 Part I.

--- day_22/main.cpp
+++ day_22/main.cpp
@@ -5,8 +5,29 @@
 #include <stdexcept>
 #include <array>
 #include <tuple>
+#include <unordered_map>
 
 #include "../common/utils.hpp"
+
+struct Cuboid
+{
+    int x = 0, y = 0, z = 0;
+
+    bool operator==(Cuboid const &c) const {
+        return x == c.x && y == c.y && z == c.z;
+    }
+
+    struct Hasher
+    {
+        std::size_t operator()(const Cuboid& c) const
+        {
+            size_t h1 = std::hash<double>()(c.x);
+            size_t h2 = std::hash<double>()(c.y);
+            size_t h3 = std::hash<double>()(c.z);
+            return (h1 ^ (h2 << 1)) ^ h3;
+        }
+    };
+};
 
 class Cube
 {
@@ -38,25 +59,25 @@ public:
 class CuboidsGrid
 {
 private:
-    std::vector<std::vector<std::vector<uint8_t>>> _grid;
+    std::unordered_map<Cuboid, uint8_t, Cuboid::Hasher> _cuboids;
 
     int _x_min = 0, _y_min = 0, _z_min = 0;
 
 public:
-    CuboidsGrid() = delete;
-    CuboidsGrid(std::array<int, 2> x_limits, std::array<int, 2> y_limits, std::array<int, 2> z_limits) : _x_min(x_limits[0]), _y_min(y_limits[0]), _z_min(z_limits[0]), _grid(
-        z_limits[1] - z_limits[0] + 1, std::vector<std::vector<uint8_t>>(
-            y_limits[1] - y_limits[0] + 1, std::vector<uint8_t>(
-                x_limits[1] - x_limits[0] + 1, 0
-            )
-        )
-    ) {}
 
     void place_cube(const Cube& cube, uint8_t action) {
-        for (int x = cube.x_range[0]; x <= cube.x_range[1]; x++) {
-            for (int y = cube.y_range[0]; y <= cube.y_range[1]; y++) {
-                for (int z = cube.z_range[0]; z <= cube.z_range[1]; z++) {
-                    _grid[z-_z_min][y-_y_min][x-_x_min] = action;
+        if (cube.x_range[1] < -50 || cube.x_range[0] > 50) return;
+        if (cube.y_range[1] < -50 || cube.y_range[0] > 50) return;
+        if (cube.z_range[1] < -50 || cube.z_range[0] > 50) return;
+
+        std::array<int, 2> x_range { cube.x_range[0] < -50 ? -50 : cube.x_range[0], cube.x_range[1] > 50 ? 50 : cube.x_range[1]};
+        std::array<int, 2> y_range { cube.y_range[0] < -50 ? -50 : cube.y_range[0], cube.y_range[1] > 50 ? 50 : cube.y_range[1]};
+        std::array<int, 2> z_range { cube.z_range[0] < -50 ? -50 : cube.z_range[0], cube.z_range[1] > 50 ? 50 : cube.z_range[1]};
+
+        for (int x = x_range[0]; x <= x_range[1]; x++) {
+            for (int y = y_range[0]; y <= y_range[1]; y++) {
+                for (int z = z_range[0]; z <= z_range[1]; z++) {
+                    _cuboids[{x,y,z}] = action;
                 }
             }
         }
@@ -66,12 +87,8 @@ public:
     int count_cuboids() {
         int counter = 0;
 
-        for (const auto & matrix : _grid) {
-            for (const auto & row : matrix) {
-                for (const auto & value : row) {
-                    if (value) counter++;
-                }
-            }
+        for (const auto & cuboid : _cuboids) {
+            if (cuboid.second) counter++;
         }
 
         return counter;
@@ -109,30 +126,13 @@ int main(int argc, char* argv[]) {
     int result_1 = 0;
 
     float part_1_elapsed_time = time_block( [&](){
-        std::array<int, 2> x_range {0,0}, y_range {0,0}, z_range {0,0};
-
-        for (const auto & cube : CUBES) {
-            if (cube.x_range[0] < x_range[0]) x_range[0] = cube.x_range[0];
-            if (cube.x_range[1] > x_range[1]) x_range[1] = cube.x_range[1];
-            if (cube.y_range[0] < y_range[0]) y_range[0] = cube.y_range[0];
-            if (cube.y_range[1] > y_range[1]) y_range[1] = cube.y_range[1];
-            if (cube.z_range[0] < z_range[0]) z_range[0] = cube.z_range[0];
-            if (cube.z_range[1] > z_range[1]) z_range[1] = cube.z_range[1];
-        }
-
-        printf("X: %d %d\n", x_range[0], x_range[1]);
-        printf("y: %d %d\n", y_range[0], y_range[1]);
-        printf("z: %d %d\n", z_range[0], z_range[1]);
-        uint64_t total = (x_range[1]-x_range[0]+1) * (y_range[1]-y_range[0]+1) * (z_range[1]-z_range[0]+1);
-        printf("TOTAL: %lu\n", total);
-
-        // CuboidsGrid grid(x_range, y_range, z_range);
+        CuboidsGrid grid;
 
         for (int i = 0 ; i < ACTIONS.size() ; i++) {
-            // grid.place_cube(CUBES[i], ACTIONS[i]);
+            grid.place_cube(CUBES[i], ACTIONS[i]);
         }
 
-        // result_1 = grid.count_cuboids();
+        result_1 = grid.count_cuboids();
     });
     
     // Part One visualization
